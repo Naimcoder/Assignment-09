23a3c855f6e95ec30c28f0cc273e4e735533c35f
Naimcoder <isteiaknaim12@gmail.com>
2022-10-10 23:50:39 +0600

    added banner part

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { CatagoryContext } from '../../Layout/Root';
+import Head from '../Head/Head';
 import Quiz from '../Quiz/Quiz';
 
 
@@ -7,8 +8,12 @@ const Home = () => {
   const catagoryQuiz= useContext(CatagoryContext)
   console.log(catagoryQuiz)
     return (
-        <div className='grid grid-cols-2 gap-4 w-10/12 mx-auto my-20'>
-            {catagoryQuiz.map(quiz=><Quiz key={quiz.id} quiz={quiz}></Quiz>)}
+        <div className=''>
+            <Head></Head>
+            <div className='grid grid-cols-2 gap-4 w-10/12 mx-auto my-20'>
+                   {catagoryQuiz.map(quiz=><Quiz key={quiz.id} quiz={quiz}></Quiz>)}
+            </div>
+          
         </div>
     );
 };
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Quiz = ({quiz}) => {
-
+const {id,name,logo,total}=quiz;
     return (
         <div>
-            
+            <img src={logo} alt="" />
         </div>
     );
 };
