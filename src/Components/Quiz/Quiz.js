5f8281fa4aa3d05af96a89f87dbd9590df67b32b
Naimcoder <isteiaknaim12@gmail.com>
2022-10-11 00:27:58 +0600

    added icon

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom';
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
 
 const Quiz = ({quiz}) => {
-const {id,name,logo,total}=quiz;
+const {name,logo}=quiz;
     return (
         <div className=' bg-slate-800 shadow-xl rounded'>
             <img className=' p-10 w-30 h-50 mx-auto' src={logo} alt="" />
              <div className="p-5">
               <h2 className=' text-white uppercase font-bold mb-5'>{name}</h2>
-              <Link className=' flex uppercase font-semibold  text-blue-700 ' to='/'>
-                Read More
-                 <ArrowRightIcon  className=" ml-2 h-6 w-6 text-blue-500"/>
-              </Link>
+               <Link
+              to="/"
+              className="inline-flex uppercase items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide bg-black text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
+            >
+              Read more
+                 <ArrowRightIcon className="hover:text-white ml-2 h-6 w-6 text-blue-500"/>
+            </Link>
              </div>
         </div>
     );
