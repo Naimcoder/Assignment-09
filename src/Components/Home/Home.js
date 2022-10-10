1eab8433b345270e53848c33109c670650b3f7cd
Naimcoder <isteiaknaim12@gmail.com>
2022-10-11 00:10:38 +0600

    added card part

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     return (
         <div className=''>
             <Head></Head>
-            <div className='grid grid-cols-2 gap-4 w-10/12 mx-auto my-20'>
+            <div className='grid grid-cols-4 gap-4 w-10/12 mx-auto my-20'>
                    {catagoryQuiz.map(quiz=><Quiz key={quiz.id} quiz={quiz}></Quiz>)}
             </div>
           
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Quiz = ({quiz}) => {
 const {id,name,logo,total}=quiz;
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className=' bg-slate-800 shadow-xl rounded'>
+            <img className=' p-10 w-30 h-50 mx-auto' src={logo} alt="" />
+             <div className="p-5">
+              <h2 className=' text-white uppercase font-bold'>{name}</h2>
+              <Link to='/'>
+                <button className='  uppercase font-semibold  text-blue-700'>Read More</button>
+              </Link>
+             </div>
         </div>
     );
 };
