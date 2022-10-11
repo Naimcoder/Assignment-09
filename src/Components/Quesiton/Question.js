fe008ea5ac63db173825f76915349e7634ea928b
Naimcoder <isteiaknaim12@gmail.com>
2022-10-11 14:03:14 +0600

    added all rechart part

--- src/Components/Quesiton/Question.js
+++ src/Components/Quesiton/Question.js
@@ -13,17 +13,18 @@ const Question = ({pd}) => {
        alert('worng')
         }
     }
-    // const hendlerView=(pd)=>{
-    //     console.log(pd)
-    // }
+    const viewhendlr= ()=>{
+      alert(correctAnswer)
+    }
+    
     return (
         <div className='  mx-auto bg-slate-50 p-5 rounded-lg text-base  my-5 w-8/12 drop-shadow-lg'>
            
             <h3 className=' flex  justify-between text-center pt-3 pb-5 font-bold sm:text-base'>
                {pd.question}
-                <EyeIcon  className=" right-0 hover:text-black ml-2 h-6 w-6 text-blue-500"/>
+                <EyeIcon onClick={viewhendlr} className=" right-0 hover:text-black ml-2 h-6 w-6 text-blue-500"/>
             </h3>
-            <div className='grid grid-cols-2 gap-3'>
+            <div className='grid md:grid-cols-2 gap-3'>
                 {pd.options.map(option=>
 
                 <label  key={option} htmlFor={option} className='ml-3 border p-3 m-2 flex bg-white rounded'>
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LineChart,AreaChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Area, PolarGrid } from 'recharts';
+import { CatagoryContext } from '../../Layout/Root';
 
 const Statistics = () => {
-    return (
-        <div>
-            <h2>this is a Statistics</h2>
-        </div>
+    const itesm= useContext(CatagoryContext)
+ return (
+    <div className='m-auto mt-20'>
+        <h2 className='text-center text-4xl font-bold'>Quiz Total Analytics</h2>
+    <ResponsiveContainer width='80%'height={500}>
+         <AreaChart className='m-10' width={700}height={500} data={itesm}>
+             <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+            
+             <XAxis dataKey="name" />
+             <YAxis dataKey="total"/>
+             <Tooltip></Tooltip>
+            </AreaChart>
+    </ResponsiveContainer>
+    </div>
+   
+    
     );
 };
 
